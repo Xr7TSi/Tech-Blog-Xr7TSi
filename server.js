7c2b3f627d71f93023f425fde6614ab4fe9ee189
Jonathan Rein <jrein1296@gmail.com>
2021-05-17 16:33:41 -0400

    Push before AskBCS support. Error: Cannot find module '../models'.

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
+const path = require('path');
 const express = require("express");
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require("./routes");
+const hbs = exphbs.create({});
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const routes = require("./controllers");
 const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up sessions with cookies
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/blog-routes'));
+app.use(routes);
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
