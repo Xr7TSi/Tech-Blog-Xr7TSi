152eede4fb9fc9cd006a2ce4e212bbcf01d09577
Jonathan Rein <jrein1296@gmail.com>
2021-05-17 14:51:22 -0400

    Adding bcrypt to controllers/routes.js.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require("./routes");
 const sequelize = require("./config/connection");
