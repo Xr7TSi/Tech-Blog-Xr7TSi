ed09d47e28b6fd7c244d5d03e29e9b8b45104323
Jonathan Rein <jrein1296@gmail.com>
2021-05-17 21:54:51 -0400

    Updates to connection.js, correcting password error.

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,16 @@
-const Sequelize = require('sequelize');
-
-// Enable access to .env variables
 require('dotenv').config();
 
-// Use environment variables to connect to database
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  }
-);
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
