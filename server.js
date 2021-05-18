3f9c84b43b480acbc165e067f3d288f34db2d697
Jonathan Rein <jrein1296@gmail.com>
2021-05-18 12:43:08 -0400

    Cannot post new user.  Error messag= syntax error.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const bodyParser = require('body-parser');
 
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
@@ -29,6 +30,12 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
