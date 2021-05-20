0f3a3dbd75d9dcdfbeb05d1c2dfda554d3eb70a8
Jonathan Rein <jrein1296@gmail.com>
2021-05-20 16:25:17 -0400

    Login from home page works.  Logout button does not present after login.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,16 +35,11 @@ router.get('/login', (req, res) => {
 });
 
 
-// logout
-router.post('/logout', (req, res) => {
-  if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
-});
+
+
+
+
+
 
 
 
