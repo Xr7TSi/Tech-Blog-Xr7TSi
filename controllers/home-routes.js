a224b96519bda1df7ba0c3787423dd542c1b889d
Jonathan Rein <jrein1296@gmail.com>
2021-05-20 14:38:19 -0400

    Cannot reach /logout.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -24,6 +24,29 @@ router.get("/", async (req, res) => {
   }
 });
 
+// login
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+
+// logout
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+
 
 
 module.exports = router;
